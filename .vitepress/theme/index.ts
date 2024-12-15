37fbd8da752a9706f25ec698919199fdc4cb6a46
yasha <pesar369@gmail.com>
2024-12-15 19:34:07 +0330

    chore: add vercel analytics

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,6 +1,9 @@
+import { inject } from '@vercel/analytics';
 import Layout from "./components/Layout.vue"
 import type { Theme } from "vitepress"
 import "./assets/style.css"
+
+inject();
 
 export default {
   Layout,
